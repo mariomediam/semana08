4d1f2e231fd63c7883517841ae02e7b807fc3404
mariomediam <54757091+mariomediam@users.noreply.github.com>
2021-10-13 15:27:58 -0500

    chore: set postgres

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,6 +1,19 @@
-import {Sequelize} from "sequelize"
+import { Sequelize } from "sequelize";
+require("dotenv").config();
 
 export const conexion = new Sequelize(
-    "postgresql://postgres:1versitario@localhost:5432/tareas",
-    {logging:false}
-)
+  process.env.DATABASE_URL,
+  {
+    logging: false,
+    // para poder usar bd remotas de postgres que requiran configuracion especial
+    dialectOptions:
+      process.env.NODE_ENV === "production" // variable que define heroku en sus servidores indicando el contenido de production
+        ? {
+            // sirve para indicar que la bd no sea necesario que tenga certificado SSL y si es que no lo tiene con el rejectUnauthorized = false  indicaremos que no emita el error y que normal se conecte.
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+  } // logging => indica si que quiere mostrar o no las consultas a la bd
+);
